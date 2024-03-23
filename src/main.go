aa33f04f0b565e761b5f24bbe874fef7a3a5f74e
kitakou0313 <kitakou0313@gmail.com>
2024-03-23 04:58:36 +0000

    fix:add output

--- src/main.go
+++ src/main.go
@@ -1,23 +1,63 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func printMeminfo() {
+	data, err := os.ReadFile("/proc/meminfo")
+	if err != nil {
+		fmt.Println("ファイルの読み込みエラー:", err)
+		return
+	}
+
+	// 読み込んだデータを文字列に変換
+	meminfoStr := string(data)
+
+	// 改行で分割して各行を表示
+	lines := strings.Split(meminfoStr, "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+}
+
+func printMemoryStat() {
+	data, err := os.ReadFile("/sys/fs/cgroup/memory/memory.stat")
+	if err != nil {
+		fmt.Println("ファイルの読み込みエラー:", err)
+		return
+	}
+
+	// 読み込んだデータを文字列に変換
+	meminfoStr := string(data)
+
+	// 改行で分割して各行を表示
+	lines := strings.Split(meminfoStr, "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+}
+
 func main() {
-	timerForMalloc := time.NewTicker(time.Second * 5)
+	mallocInterval, err := strconv.Atoi(os.Getenv("MALLOC_INTERVAL_MILLISECOND"))
+	if err != nil {
+		panic(err)
+	}
+
+	timerForMalloc := time.NewTicker(time.Millisecond * time.Duration(mallocInterval))
 	memory := make(map[int][]byte)
 
 	i := 0
-	go func() {
-		for range timerForMalloc.C {
-			memory[i] = make([]byte, 1024)
-		}
-	}()
 
-	http.Handle("/metric", promhttp.Handler())
-	http.ListenAndServe(":8081", nil)
+	for range timerForMalloc.C {
+		memory[i] = make([]byte, 1024*1024)
+		i += 1
+		printMeminfo()
+		printMemoryStat()
+	}
+
 }
