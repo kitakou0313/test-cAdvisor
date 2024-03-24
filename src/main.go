10fc62613e47b8d7bf72751de26b7642ff41cbc6
kitakou0313 <kitakou0313@gmail.com>
2024-03-24 13:18:33 +0000

    add:add dir for log

--- src/main.go
+++ src/main.go
@@ -4,42 +4,47 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 )
 
-func printMeminfo() {
+func writeToFile(data string) {
+	file, err := os.OpenFile("/var/log/myapp/log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("ファイルを開く際にエラーが発生しました:", err)
+		return
+	}
+	defer file.Close()
+
+	// ファイルにデータを書き込む
+	_, err = file.WriteString(data)
+	if err != nil {
+		fmt.Println("ファイルに書き込む際にエラーが発生しました:", err)
+		return
+	}
+
+	fmt.Println("ファイルにデータを追記しました")
+}
+
+func readMeminfo() string {
 	data, err := os.ReadFile("/proc/meminfo")
 	if err != nil {
 		fmt.Println("ファイルの読み込みエラー:", err)
-		return
+		return ""
 	}
 
 	// 読み込んだデータを文字列に変換
-	meminfoStr := string(data)
-
-	// 改行で分割して各行を表示
-	lines := strings.Split(meminfoStr, "\n")
-	for _, line := range lines {
-		fmt.Println(line)
-	}
+	return string(data)
 }
 
-func printMemoryStat() {
+func readMemoryStat() string {
 	data, err := os.ReadFile("/sys/fs/cgroup/memory/memory.stat")
 	if err != nil {
 		fmt.Println("ファイルの読み込みエラー:", err)
-		return
+		return ""
 	}
 
 	// 読み込んだデータを文字列に変換
-	meminfoStr := string(data)
-
-	// 改行で分割して各行を表示
-	lines := strings.Split(meminfoStr, "\n")
-	for _, line := range lines {
-		fmt.Println(line)
-	}
+	return string(data)
 }
 
 func main() {
@@ -56,8 +61,8 @@ func main() {
 	for range timerForMalloc.C {
 		memory[i] = make([]byte, 1024*1024)
 		i += 1
-		printMeminfo()
-		printMemoryStat()
+		writeToFile(readMeminfo())
+		writeToFile(readMemoryStat())
 	}
 
 }
